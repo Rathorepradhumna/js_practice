c29510e2114373cd2c1180db200f3dd8bfeb0a58
Rathorepradhumna <42332858+Rathorepradhumna@users.noreply.github.com>
2020-09-02 00:37:28 +0530

    example to show logging

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-import {create as newBlogPost} from "blogspot.js";
+const logger = require('./logger.js');
+const { Logform } = require('winston');
+const { resolve } = require('path');
 
-var forAgainstLet = newBlogPost{
-    "for and against let",
-    "pradhumna rathore",
-    "aug 26,1998",
-    "http:google.com"
-};
-forAgainstLet.print();
+function func1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const error = true;
+            if (!error) {
+                resolve("your problem has been resolved");
+
+            }
+            else {
+                reject("your problem has been rejected")
+            }
+        }, 3000)
+    })
+}
+func1().then((data)=>{logger.info(data)}).catch((data)=>{logger.info(data)})
